230410a6168baf9b9f1ea88b1ec8ea083e362cb2
iDUCK srl <plugins@jlm.srl>
2024-05-12 05:30:55 +0000

    improve: formatting

--- includes/IDER_Rewrites.php
+++ includes/IDER_Rewrites.php
@@ -7,21 +7,17 @@
  * @subpackage  Ider
  * @author      Davide Lattanzio <plugins@jlm.srl>
  * @since       1.0
- *
  */
 
 defined('ABSPATH') or die('No script kiddies please!');
-
 
 use IDERConnect\IDEROpenIDClient;
 
 /**
  * Class WPOSSO_Rewrites
- *
  */
 class IDER_Rewrites
 {
-
     static function init()
     {
 
@@ -30,7 +26,6 @@ class IDER_Rewrites
         add_filter('wp_loaded', array(__CLASS__, 'flush_rewrite_rules'));
         add_action('template_redirect', array(__CLASS__, 'template_redirect_intercept'));
     }
-
 
     static function create_rewrite_rules($rules)
     {
@@ -41,7 +36,6 @@ class IDER_Rewrites
         return $newRules;
     }
 
-
     static function add_query_vars($qvars)
     {
         $qvars[] = 'auth';
@@ -49,29 +43,21 @@ class IDER_Rewrites
         return $qvars;
     }
 
-
     static function flush_rewrite_rules()
     {
         global $wp_rewrite;
         $wp_rewrite->flush_rules();
     }
 
-
-    /**
-     *
-     */
     static function template_redirect_intercept()
     {
         global $wp_query;
 
         // echo $wp_query->get('auth'); exit;
 
-
         // if auth code or callback: pass the control to library
         if ('ider' == $wp_query->get('auth') or $wp_query->get('name') == IDEROpenIDClient::$IDERRedirectURL or $wp_query->get('name') == IDEROpenIDClient::$IDERButtonURL) {
            IDER_Server::IDerOpenIdClientHandler();
         }
-
-
     }
 }
